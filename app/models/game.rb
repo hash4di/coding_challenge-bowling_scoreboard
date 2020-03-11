520ea531b78d6714a6bbe629490a210686ab8c03
Adrian Checinski <hash4di@gmail.com>
2020-03-11 14:40:27 +0100

    Add basic properties for Game object
    Empty frame and flattened score.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,12 @@
 class Game < ApplicationRecord
+    serialize :frames, Array
+
+    after_initialize do
+        self.frames = [[]]
+      end
+
+    before_save do
+        self.score = frames.flatten.sum
+    end
+
 end
