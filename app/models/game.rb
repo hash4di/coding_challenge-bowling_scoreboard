a914797bcd6e13d85f01323f08c8581e9a7b3975
Adrian Checinski <hash4di@gmail.com>
2020-03-11 15:10:55 +0100

    Fix fals 4 signs TAB indention.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,17 +1,17 @@
 class Game < ApplicationRecord
-    serialize :frames, Array
+  serialize :frames, Array
 
-    after_initialize do
-        self.frames = [[]]
-      end
+  after_initialize do
+      self.frames = [[]]
+  end
 
-    before_save do
-        self.score = frames.flatten.sum
-    end
+  before_save do
+    self.score = frames.flatten.sum
+  end
 
-    def throw!(knocked_pins)
-        frames.last << knocked_pins
-        save!
-      end
+  def throw!(knocked_pins)
+    frames.last << knocked_pins
+    save!
+  end
 
 end
