1428558bd4fc79c8e9c8db5ade3c6eec0d4f058f
Adrian Checinski <hash4di@gmail.com>
2020-03-11 14:43:58 +0100

    Add test for throw functionality.
    Two throws for one frame.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Game, type: :model do
     expect(Game.new.score).to eq 0
     expect(Game.new.frames).to eq [[]]
   end
+
+  context "two throws for one frame" do
+    let(:game) { Game.new }
+
+    it "should show correct summed score from two throws" do
+      game.throw!(4)
+      expect(game.score).to eq 4
+      game.throw!(3)
+      expect(game.score).to eq 7
+
+      expect(game.frames).to eq [[4,3]]
+    end
+  end
+
 end
