c018cd643697277792df8ba514b12cf92e3eaa06
Adrian Checinski <hash4di@gmail.com>
2020-03-11 16:08:41 +0100

    Add strike and spare functionality.
    With small code refactoring.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  let(:game) { Game.new }
+
   it "should have score and frames" do
-    expect(Game.new.score).to eq 0
-    expect(Game.new.frames).to eq [[]]
+    expect(game.score).to eq 0
+    expect(game.frames).to eq []
   end
 
-  context "two throws for one frame" do
-    let(:game) { Game.new }
-
+  context "with one frame" do
     it "should show correct summed score from two throws" do
       game.throw!(4)
       expect(game.score).to eq 4
@@ -16,6 +16,49 @@ RSpec.describe Game, type: :model do
       expect(game.score).to eq 7
 
       expect(game.frames).to eq [[4,3]]
+    end
+  end
+
+  context "with multiple frames" do
+    it "should show correct summed score from four throws" do
+      game.throw! 4
+      expect(game.score).to eq 4
+      game.throw! 3
+
+      expect(game.score).to eq 7
+      expect(game.frames).to eq [[4,3]]
+
+      game.throw! 6
+      expect(game.score).to eq 13
+      game.throw! 3
+        
+      expect(game.score).to eq 16
+      expect(game.frames).to eq [[4,3], [6,3]]
+    end
+
+    context "with a strike" do
+      it "should have correct score with a strike at first frame" do
+        game.throw! 10
+        expect(game.score).to eq 10
+
+        game.throw! 4
+        game.throw! 4
+        expect(game.score).to eq 26
+        expect(game.frames).to eq [[10, 4, 4], [4,4]]
+      end
+    end
+
+    context "with a spare" do
+      it "should have correct score with a spare at first frame" do
+        game.throw! 3
+        game.throw! 7
+        expect(game.score).to eq 10
+
+        game.throw! 4
+        game.throw! 4
+        expect(game.score).to eq 22
+        expect(game.frames).to eq [[3, 7, 4], [4,4]]
+      end
     end
   end
 
