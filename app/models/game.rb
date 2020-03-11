f41c9beba0e1de2fa11e83e282674a3c6e2aca64
Adrian Checinski <hash4di@gmail.com>
2020-03-11 16:09:22 +0100

    Add multi-frames option and strike spare.
    Code refactoring in Game model.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,17 +1,30 @@
 class Game < ApplicationRecord
   serialize :frames, Array
 
-  after_initialize do
-      self.frames = [[]]
-  end
-
   before_save do
     self.score = frames.flatten.sum
   end
 
-  def throw!(knocked_pins)
+  def throw! knocked_pins 
+    frames << [] if frame_completed?(frames.last)
     frames.last << knocked_pins
+    complete_previous_open_frame_with knocked_pins
     save!
   end
+
+  private
+
+    def frame_completed? frame
+      return true if (frame.nil? || frame.size==2 || frame[0]==10)
+      false
+    end
+
+    def complete_previous_open_frame_with value
+      frames.map do |frame|
+        next if frame == frames.last
+        frame << value if frame[0]==10 && frame.size <= 2 #strike = knocked 10 pins with 1st throw
+        frame << value if (frame[0]+frame[1])==10 && frame.size == 2 #spare = knocked 10 pins in two throws
+      end
+    end
 
 end
